7d6f729bc6ec4c60ed5891d100d2104a7a6d20ba
Michal Čihař <michal@cihar.com>
2016-06-10 12:34:58 +0200

    Fix PHP configuration name
    Fixes #32
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- run.sh
+++ run.sh
@@ -12,4 +12,4 @@ exec php -S 0.0.0.0:80 -t /www/ \
     -d upload_max_filesize=$PHP_UPLOAD_MAX_FILESIZE \
     -d post_max_size=$PHP_UPLOAD_MAX_FILESIZE \
     -d max_input_vars=$PHP_MAX_INPUT_VARS \
-    -d session.save-path=/sessions
+    -d session.save_path=/sessions
