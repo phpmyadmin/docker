ef53180e087308fc9161f8c4f973cb004be4cc24
Michal Čihař <michal@cihar.com>
2016-07-26 11:56:47 +0200

    Increase number of chars allowed in secret
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -x
 if [ ! -f /www/config.secret.inc.php ] ; then
     cat > /www/config.secret.inc.php <<EOT
 <?php
-\$cfg['blowfish_secret'] = '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`';
+\$cfg['blowfish_secret'] = '`cat /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' | fold -w 32 | head -n 1`';
 EOT
 fi
 
