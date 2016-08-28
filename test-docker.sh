a1c139f90181687fcca0fbd3e2ede879a301276b
Michal Čihař <michal@cihar.com>
2016-08-28 09:47:09 +0200

    Debug failures
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test-docker.sh
+++ test-docker.sh
@@ -9,6 +9,9 @@ PORT=$2
 while ! docker exec $NAME ps aux | grep -q nginx ; do echo 'Waiting for start...'; sleep 1; done
 docker ps -a
 docker exec $NAME ps faux
+curl http://127.0.0.1:$PORT/
+docker exec $NAME cat /var/log/php7.0-fpm.log
+docker exec $NAME cat /var/log/nginx-error.log
 curl http://127.0.0.1:$PORT/ | grep -q input_password
 curl --cookie-jar /tmp/cj --location -d pma_username=root -d pma_password=my-secret-pw -d server=1 http://127.0.0.1:$PORT/ | grep -r 'db via TCP'
 docker exec $NAME cat /var/log/php7.0-fpm.log
