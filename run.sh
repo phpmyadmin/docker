b2c50758769079da40353885db6647a071d354fd
Michal Čihař <michal@cihar.com>
2016-10-18 12:39:18 +0200

    Avoid encoding PHP version in socket or log name
    Fixes #68
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- run.sh
+++ run.sh
@@ -14,8 +14,8 @@ mkdir -p /var/nginx/client_body_temp
 chown nobody:nobody /sessions /var/nginx/client_body_temp
 mkdir -p /var/run/php/
 chown nobody:nobody /var/run/php/
-touch /var/log/php7.0-fpm.log
-chown nobody:nobody /var/log/php7.0-fpm.log
+touch /var/log/php-fpm.log
+chown nobody:nobody /var/log/php-fpm.log
 
 if [ "$1" = 'phpmyadmin' ]; then
     exec supervisord --nodaemon --configuration="/etc/supervisord.conf" --loglevel=info
