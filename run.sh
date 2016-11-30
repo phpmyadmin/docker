c5c62ee24b176206465a9897e736f68b0591269c
Michal Čihař <michal@cihar.com>
2016-11-30 11:17:43 +0100

    Create secret and user configs in /etc/phpmyadmin
    Fixes #95
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if [ ! -f /www/config.secret.inc.php ] ; then
-    cat > /www/config.secret.inc.php <<EOT
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ] ; then
+    cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
 <?php
 \$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
 EOT
 fi
 
-if [ ! -f /www/config.user.inc.php ] ; then
-  touch /www/config.user.inc.php
+if [ ! -f /etc/phpmyadmin/config.user.inc.php ] ; then
+  touch /etc/phpmyadmin/config.user.inc.php
 fi
 
 mkdir -p /var/nginx/client_body_temp
