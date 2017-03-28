5646cff98e44e3362df4bf3818ab0c1d679453b0
Michal Čihař <michal@cihar.com>
2017-03-28 15:09:20 +0200

    Do not report retries
    This is expected to take some time...
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- testing/wait-for-testing.sh
+++ testing/wait-for-testing.sh
@@ -12,7 +12,6 @@ TIMEOUT=0
 sleep 5
 
 while [ `docker-compose -f docker-compose.testing.yml logs phpmyadmin | grep -c 'Result of \(mysql\|mariadb\) tests'` -lt 2 ] ; do
-    echo Retry...
     sleep 1
     TIMEOUT=$(($TIMEOUT + 1))
     if [ $TIMEOUT -gt 20 ] ; then
