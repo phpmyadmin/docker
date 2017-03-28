382f9b9489bbe312073472b63f4eeb01f9b412de
Michal Čihař <michal@cihar.com>
2017-03-28 15:07:27 +0200

    better progress reporting
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- testing/wait-for-testing.sh
+++ testing/wait-for-testing.sh
@@ -3,6 +3,10 @@
 # Helper script to wait for testing inside container completes
 # Needs running docker-compose container based on docker-compose.testing.yml
 
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NC="\033[0m" # No Color
+
 TIMEOUT=0
 
 sleep 5
@@ -13,15 +17,17 @@ while [ `docker-compose -f docker-compose.testing.yml logs phpmyadmin | grep -c 
     TIMEOUT=$(($TIMEOUT + 1))
     if [ $TIMEOUT -gt 20 ] ; then
         docker-compose -f docker-compose.testing.yml logs phpmyadmin
-        echo 'Timeout!'
+        echo "${RED}Timeout!${NC}"
         exit 1
     fi
 done
 
 if [ `docker-compose -f docker-compose.testing.yml logs phpmyadmin | grep -c 'Result of \(mysql\|mariadb\) tests.*SUCCESS'` -lt 2 ] ; then
     docker-compose -f docker-compose.testing.yml logs phpmyadmin
-    echo 'Failed!'
+    echo "${RED}Failed${NC}"
     exit 2
 fi
 
+docker-compose -f docker-compose.testing.yml logs phpmyadmin
+echo "${GREEN}Success{$NC}"
 exit 0
