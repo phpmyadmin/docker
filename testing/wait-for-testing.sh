6314b17ebd0f043f4fca319bb3a37ba58db8dd6d
Michal Čihař <michal@cihar.com>
2017-03-28 15:01:34 +0200

    Need more time to run tests
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- testing/wait-for-testing.sh
+++ testing/wait-for-testing.sh
@@ -5,11 +5,13 @@
 
 TIMEOUT=0
 
+sleep 5
+
 while [ `docker-compose -f docker-compose.testing.yml logs phpmyadmin | grep -c 'Result of \(mysql\|mariadb\) tests'` -lt 2 ] ; do
     echo Retry...
     sleep 1
     TIMEOUT=$(($TIMEOUT + 1))
-    if [ $TIMEOUT -gt 10 ] ; then
+    if [ $TIMEOUT -gt 20 ] ; then
         docker-compose -f docker-compose.testing.yml logs phpmyadmin
         echo 'Timeout!'
         exit 1
