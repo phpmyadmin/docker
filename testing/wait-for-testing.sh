63fd59434eb9e00ec3de940610b7378f2b9876ce
Michal Čihař <michal@cihar.com>
2017-03-28 14:51:04 +0200

    Display logs on timeout
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- testing/wait-for-testing.sh
+++ testing/wait-for-testing.sh
@@ -10,6 +10,7 @@ while [ `docker-compose -f docker-compose.testing.yml logs phpmyadmin | grep -c 
     sleep 1
     TIMEOUT=$(($TIMEOUT + 1))
     if [ $TIMEOUT -gt 10 ] ; then
+        docker-compose -f docker-compose.testing.yml logs phpmyadmin
         echo 'Timeout!'
         exit 1
     fi
