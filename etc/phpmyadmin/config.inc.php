1204bcd696b1fe969dea7d62f56134ef5dbfaf97
Michal Čihař <michal@cihar.com>
2017-04-11 17:18:26 +0200

    Do not set password to null when not specified
    This makes the authentication code think this parameter was not set at
    all.
    
    Fixes #118
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- etc/phpmyadmin/config.inc.php
+++ etc/phpmyadmin/config.inc.php
@@ -59,7 +59,7 @@ for ($i = 1; isset($hosts[$i - 1]); $i++) {
     if (isset($_ENV['PMA_USER'])) {
         $cfg['Servers'][$i]['auth_type'] = 'config';
         $cfg['Servers'][$i]['user'] = $_ENV['PMA_USER'];
-        $cfg['Servers'][$i]['password'] = isset($_ENV['PMA_PASSWORD']) ? $_ENV['PMA_PASSWORD'] : null;
+        $cfg['Servers'][$i]['password'] = isset($_ENV['PMA_PASSWORD']) ? $_ENV['PMA_PASSWORD'] : '';
     } else {
         $cfg['Servers'][$i]['auth_type'] = 'cookie';
     }
