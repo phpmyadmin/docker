df45b3f76cb70e46c34813ba786fb6b45e8e4b66
J0WI <J0WI@users.noreply.github.com>
2018-06-05 00:29:06 +0200

    code style

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-if [ ! -f /etc/phpmyadmin/config.secret.inc.php ] ; then
+if [ ! -f /etc/phpmyadmin/config.secret.inc.php ]; then
     cat > /etc/phpmyadmin/config.secret.inc.php <<EOT
 <?php
 \$cfg['blowfish_secret'] = '$(tr -dc 'a-zA-Z0-9~!@#$%^&*_()+}{?></";.,[]=-' < /dev/urandom | fold -w 32 | head -n 1)';
 EOT
 fi
 
-if [ ! -f /etc/phpmyadmin/config.user.inc.php ] ; then
-  touch /etc/phpmyadmin/config.user.inc.php
+if [ ! -f /etc/phpmyadmin/config.user.inc.php ]; then
+    touch /etc/phpmyadmin/config.user.inc.php
 fi
 
 mkdir -p /var/nginx/client_body_temp
