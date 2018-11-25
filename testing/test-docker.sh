b1da7f558e652272239eca70739d6ef9853fa01d
J0WI <J0WI@users.noreply.github.com>
2018-11-25 18:29:02 +0100

    Fix tests in conatiner

--- testing/test-docker.sh
+++ testing/test-docker.sh
@@ -19,20 +19,27 @@ PHPMYADMIN_HOSTNAME=${TESTSUITE_HOSTNAME:=localhost}
 PHPMYADMIN_PORT=${TESTSUITE_PORT:=80}
 PHPMYADMIN_URL=http://$PHPMYADMIN_HOSTNAME:$PORT/
 
+# Set database environment
+PHPMYADMIN_DB_HOSTNAME=${PMA_HOST:=localhost}
+PHPMYADMIN_DB_PORT=${PMA_PORT:=3306}
+
 # Color text output
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 NC="\033[0m" # No Color
 
-ret=0
-# Check if script is running inside container
-if [ -f /.dockerenv ] ; then
-    echo "Tests running inside container..."
+# Find test script
+if [ -f ./phpmyadmin_test.py ] ; then
+    FILENAME=./phpmyadmin_test.py
+else
+    FILENAME=./testing/phpmyadmin_test.py
+fi
 
-    # Set database environment
-    PHPMYADMIN_DB_HOSTNAME=${PMA_HOST:=localhost}
-    PHPMYADMIN_DB_PORT=${PMA_PORT:=3306}
-    PHPMYADMIN_DB_URL=http://$PHPMYADMIN_DB_HOSTNAME:$PHPMYADMIN_DB_PORT/
+# Check if script is running inside container
+if ! [ -f /.dockerenv ] ; then
+    echo "Tests running outside container..."
+    COMMAND_HOST="docker exec ${NAME}"
+    docker ps -a
 
     # Wait for database to start
     TIMEOUT=0
@@ -43,48 +50,38 @@ if [ -f /.dockerenv ] ; then
         if [ $TIMEOUT -gt 3 ] ; then
             echo "Failed to connect ${PHPMYADMIN_DB_HOSTNAME} database!"
             echo "Result of ${PHPMYADMIN_DB_HOSTNAME} tests: ${RED}FAILED${NC}"
-            ret=1
+            docker logs ${NAME}
+            exit 1
+        fi
+    done
+
+    # Wait for container to start
+    TIMEOUT=0
+    while [ $(docker logs ${NAME} 2>&1 | fgrep -c "Command line: 'apache2 -D FOREGROUND'") -le 0 ] ; do
+        echo "Waiting for PHPMyAdmin start..."
+        sleep 1
+        TIMEOUT=$((TIMEOUT + 1))
+        if [ $TIMEOUT -gt 10 ] ; then
+            echo "Failed to connect PHPMyAdmin!"
+            echo "Result of ${PHPMYADMIN_DB_HOSTNAME} tests: ${RED}FAILED${NC}"
+            docker logs ${NAME}
             exit 1
         fi
     done
 else
-    echo "Tests running outside container..."
-    docker ps -a
-    COMMAND_HOST="docker exec ${NAME}"
+    echo "Tests running inside container..."
+    FILENAME=/phpmyadmin_test.py
 fi
-
-# Wait for container to start
-TIMEOUT=0
-while [ $(docker logs ${NAME} 2>&1 | fgrep -c "Command line: 'apache2 -D FOREGROUND'") -le 0 ] ; do
-    echo "Waiting for PHPMyAdmin start..."
-    sleep 1
-    TIMEOUT=$((TIMEOUT + 1))
-    if [ $TIMEOUT -gt 10 ] ; then
-        echo "Failed to connect PHPMyAdmin!"
-        echo "Result of ${PHPMYADMIN_DB_HOSTNAME} tests: ${RED}FAILED${NC}"
-        ret=1
-        exit 1
-    fi
-done
 
 # Perform tests
-if [ $ret -eq 0 ] ; then
-    if [ -f /phpmyadmin_test.py ] ; then
-        FILENAME=/phpmyadmin_test.py
-    elif [ -f ./phpmyadmin_test.py ] ; then
-        FILENAME=./phpmyadmin_test.py
-    else
-        FILENAME=./testing/phpmyadmin_test.py
-    fi
-    python2 $FILENAME --url "$PHPMYADMIN_URL" --username root --password $TESTSUITE_PASSWORD $SERVER
-    ret=$?
-fi
+ret=0
+$FILENAME --url "$PHPMYADMIN_URL" --username root --password $TESTSUITE_PASSWORD $SERVER
+ret=$?
 
 # Show debug output in case of failure
 if [ $ret -ne 0 ] ; then
     curl "$PHPMYADMIN_URL"
-    $COMMAND_HOST ps faux
-    docker logs ${NAME}
+    ${COMMAND_HOST} ps faux
     echo "Result of ${PHPMYADMIN_DB_HOSTNAME} tests: ${RED}FAILED${NC}"
     exit $ret
 fi
