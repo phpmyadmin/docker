d147a455afce16e1470ec1337f401d7da77eda15
William Desportes <williamdes@wdes.fr>
2019-12-23 20:56:13 +0100

    Remove a non existent use case
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -57,11 +57,6 @@ UPLOAD_LIMIT_INI_FILE="$PHP_INI_DIR/conf.d/phpmyadmin-upload-limit.ini"
 if [ ! -z "${UPLOAD_LIMIT}" ]; then
     echo "Adding the custom upload limit."
     echo -e "upload_max_filesize = $UPLOAD_LIMIT\npost_max_size = $UPLOAD_LIMIT\n" > $UPLOAD_LIMIT_INI_FILE
-else
-    if [ -f $UPLOAD_LIMIT_INI_FILE ]; then
-        echo "Removing the custom upload limit."
-        rm $UPLOAD_LIMIT_INI_FILE
-    fi
 fi
 
 exec "$@"
