490df186e5255d27b481b153e9e6d1a2d3fb525e
William Desportes <williamdes@wdes.fr>
2020-05-07 08:49:22 +0200

    Add each variant to the tag list
    Signed-off-by: William Desportes <williamdes@wdes.fr>

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -81,6 +81,9 @@ for variant in apache fpm fpm-alpine; do
 		variantAliases+=( "${versionAliases[@]}" )
 	fi
 
+	# Add the variant to the tag list
+	variantAliases+=( "${variant}" )
+
 	variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$variant/Dockerfile")"
 
 	# depends on docker library
