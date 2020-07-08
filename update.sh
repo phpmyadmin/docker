494c655a620b4acc7dc5a3a26540642e0fdf49f2
Lennart Gustafsson <mail@tranos.de>
2020-07-09 01:24:24 +0200

    Simplified a lot, removed emulated templates, added custom push.

--- update.sh
+++ update.sh
@@ -21,40 +21,38 @@ declare -A base=(
 	[fpm-alpine]='alpine'
 )
 
-declare -A archlong=(
-	[arm]='arm32v7'
-	[aarch64]='arm64v8'
+declare -A arch=(
+	[arm32v7]='arm'
+	[arm64v8]='aarch64'
+	[amd64]='amd64'
 )
 
 latest="$(curl -fsSL 'https://www.phpmyadmin.net/home_page/version.json' | jq -r '.version')"
 sha256="$(curl -fsSL "https://files.phpmyadmin.net/phpMyAdmin/$latest/phpMyAdmin-$latest-all-languages.tar.xz.sha256" | cut -f1 -d ' ' | tr -cd 'a-f0-9' | cut -c 1-64)"
 
 for variant in apache fpm fpm-alpine; do
-	template="Dockerfile-${base[$variant]}.template"
-	cp $template "$variant/Dockerfile"
 	cp config.inc.php "$variant/config.inc.php"
 	cp hooks/build "$variant/hooks/build"
+	cp hooks/push "$variant/hooks/push"
 	cp hooks/pre_build "$variant/hooks/pre_build"
 	cp hooks/post_push "$variant/hooks/post_push"
 	cp multi-arch-manifest.yaml "$variant/multi-arch-manifest.yaml"
+	sed -ri -e 's/%%VARIANT%%/'"$variant"'/;' "$variant/multi-arch-manifest.yaml"
 	cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
-	for arch in arm aarch64; do
-		emulated="Dockerfile-${base[$variant]}-emulated.template"
-		cp $emulated "$variant/Dockerfile.${archlong[$arch]}"
+	for arc in arm32v7 arm64v8 amd64; do
+		template="Dockerfile-${base[$variant]}.template"
+		cp $template "$variant/Dockerfile.$arc"
 		sed -ri -e '
 		s/%%VERSION%%/'"$latest"'/;
 		s/%%SHA256%%/'"$sha256"'/;
 		s/%%VARIANT%%/'"$variant"'/;
 		s/%%CMD%%/'"${cmd[$variant]}"'/;
-		s/%%ARCH%%/'"$arch"'/g;
-		s/%%ARCHLONG%%/'"${archlong[$arch]}"'/;
-	' "$variant/Dockerfile.${archlong[$arch]}"
+		s/%%ARCH%%/'"${arch[$arc]}"'/g;
+		s/%%ARCHLONG%%/'"$arc"'/;
+		' "$variant/Dockerfile.$arc"
+		if [ $arc == amd64 ]; then
+			sed -i '/COPY qemu-amd64-static \/usr\/bin/d' "$variant/Dockerfile.$arc"
+			mv "$variant/Dockerfile.$arc" "$variant/Dockerfile"
+		fi
 	done
-	sed -ri -e 's/%%VARIANT%%/'"$variant"'/;' "$variant/multi-arch-manifest.yaml"
-	sed -ri -e '
-		s/%%VERSION%%/'"$latest"'/;
-		s/%%SHA256%%/'"$sha256"'/;
-		s/%%VARIANT%%/'"$variant"'/;
-		s/%%CMD%%/'"${cmd[$variant]}"'/;
-	' "$variant/Dockerfile"
 done
