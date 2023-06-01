2cb9a6214e1f4debe31631ad80d757136a5624f0
Daniel Rudolf <github.com@daniel-rudolf.de>
2023-06-01 16:36:36 +0200

    update.sh: Add some comments and code cleanup
    This commit includes no functional changes.
    
    Signed-off-by: Daniel Rudolf <github.com@daniel-rudolf.de>

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-set -eu -o pipefail -x
+set -eu -o pipefail
 
-# Check for dependencies
-command -v curl >/dev/null 2>&1 || { echo >&2 "'curl' is required but not found. Aborting."; exit 1; }
-command -v jq >/dev/null 2>&1 || { echo >&2 "'jq' is required but not found. Aborting."; exit 1; }
-[ -n "${BASH_VERSINFO}" ] && [ -n "${BASH_VERSINFO[0]}" ] && [ ${BASH_VERSINFO[0]} -ge 4 ] \
-	|| { echo >&2 "Bash 4.0 or greater is required. Aborting."; exit 1; }
+variants=(
+	apache
+	fpm
+	fpm-alpine
+)
+
+declare -A base=(
+	[apache]='debian'
+	[fpm]='debian'
+	[fpm-alpine]='alpine'
+)
 
 declare -A php_version=(
 	[default]='8.1'
@@ -15,12 +21,6 @@ declare -A cmd=(
 	[apache]='apache2-foreground'
 	[fpm]='php-fpm'
 	[fpm-alpine]='php-fpm'
-)
-
-declare -A base=(
-	[apache]='debian'
-	[fpm]='debian'
-	[fpm-alpine]='alpine'
 )
 
 gpg_key='3D06A59ECE730EB71B511C17CE752F178259BD92'
@@ -41,28 +41,42 @@ function create_variant() {
 
 	echo "updating $version [$branch] $variant"
 
+	# Create Dockerfile
 	local template="Dockerfile-${base[$variant]}.template"
 	cp "$template" "$variant/Dockerfile"
-	cp config.inc.php "$variant/config.inc.php"
-	cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
+
+	# Replace Dockerfile variables
 	sed -ri -e '
+		s/%%VARIANT%%/'"$variant"'/;
 		s/%%VERSION%%/'"$version"'/;
 		s/%%SHA256%%/'"$sha256"'/;
 		s/%%DOWNLOAD_URL%%/'"$(sed -e 's/[\/&]/\\&/g' <<< "$url")"'/;
 		s/%%DOWNLOAD_URL_ASC%%/'"$(sed -e 's/[\/&]/\\&/g' <<< "$ascUrl")"'/;
 		s/%%PHP_VERSION%%/'"$phpVersion"'/g;
 		s/%%GPG_KEY%%/'"$gpg_key"'/g;
-		s/%%VARIANT%%/'"$variant"'/;
 		s/%%CMD%%/'"${cmd[$variant]}"'/;
 	' "$variant/Dockerfile"
+
+	# Copy docker-entrypoint.sh
+	cp docker-entrypoint.sh "$variant/docker-entrypoint.sh"
 	if [ "$variant" != "apache" ]; then
 		sed -i "/^# start: Apache specific settings$/,/^# end: Apache specific settings$/d" "$variant/docker-entrypoint.sh"
 	fi
+
+	# Copy config.inc.php
+	cp config.inc.php "$variant/config.inc.php"
 }
 
-latest="$(curl -fsSL 'https://www.phpmyadmin.net/home_page/version.json' | jq -r '.version')"
+# Check script dependencies
+command -v curl >/dev/null 2>&1 || { echo >&2 "'curl' is required but not found. Aborting."; exit 1; }
+command -v jq >/dev/null 2>&1 || { echo >&2 "'jq' is required but not found. Aborting."; exit 1; }
+[ -n "${BASH_VERSINFO}" ] && [ -n "${BASH_VERSINFO[0]}" ] && [ ${BASH_VERSINFO[0]} -ge 4 ] \
+	|| { echo >&2 "Bash 4.0 or greater is required. Aborting."; exit 1; }
+
+# Create variants
+latest="$(curl -fsSL "https://www.phpmyadmin.net/home_page/version.json" | jq -r '.version')"
 sha256="$(curl -fsSL "$(download_url "$latest").sha256" | cut -f1 -d ' ' | tr -cd 'a-f0-9' | cut -c 1-64)"
 
-for variant in apache fpm fpm-alpine; do
+for variant in "${variants[@]}"; do
 	create_variant "$variant" "$latest" "$sha256"
 done
